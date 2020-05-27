34838b8b9318851c7a02df3c2f8bc4a8f97c5874
Kleyton João <kleyton.joao@gmail.com>
2020-05-27 13:12:14 -0300

    Config css do menu e topo da aplicacao base

--- src/app/containers/Base/index.js
+++ src/app/containers/Base/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
+const BarraTopo = () => {
+    <div className="flex horizontal full-width">
+        <div className="flex-1 flex flex-start">
+            <p>Ver loja</p>
+        </div>
+        <div className="flex-1 flex flex-end">
+            <p>Sair</p>
+        </div>
+    </div>
+}
+
 class Dashboard extends React.Component {
-    render(){
-        return(
-            <div className="flex horizotal">
+    render() {
+        return (
+            <div className="flex horizotal full-height">
                 <div className="flex vertical">
                     <p>Menu</p>
                 </div>
-                <div className="flex vertical">
+                <div className="flex vertical full-width">
                     <div className="flex horizotal">
-                        barra topo
+                        <BarraTopo />
                     </div>
-                    <div className="flex">
+                    <div className="flex full-height">
                         {this.props.children}
                     </div>
                 </div>

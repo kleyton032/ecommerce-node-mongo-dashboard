5ee162bb3b760bf2c920c3f6dc1a213f127c608b
Kleyton João <kleyton.joao@gmail.com>
2020-06-21 11:59:46 -0300

    Criacao da tela recuperar senha e resetar senha

--- src/app/containers/RecuperarSenha/ResetarSenha.js
+++ src/app/containers/RecuperarSenha/ResetarSenha.js
@@ -1,10 +1,44 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
+import Titulo from '../../components/Texto/Titulo';
+import Input from '../../components/Inputs/Simples';
+import Button from '../../components/Button/Simples';
 
 class ResetarSenha extends Component {
-    render(){
-        return(
+
+    state = {
+        senha: "",
+        confirmarSenha: ""
+    }
+
+    onChangeInput = (field, ev) => this.setState({ [field]: ev.target.value });
+
+    render() {
+        const { senha, confirmarSenha } = this.state;
+        return (
             <div className="Resetar-Senha">
-                Resetar-Senha
+                <Titulo tipo="h1" titulo="LOJA IT" />
+                <br />
+                <div>
+                    <p>Para reiniciar sua senha digite sa nova senha e confirme no campo abaixo!</p>
+                </div>
+                <br />
+                <div>
+                    <Input
+                        label="Senha"
+                        value={senha}
+                        onChange={(ev) => this.onChangeInput("senha", ev)}
+                        type="password" />
+
+                    <Input
+                        label="Confirmar Senha"
+                        value={confirmarSenha}
+                        onChange={(ev) => this.onChangeInput("confirmarSenha", ev)}
+                        type="password" />
+                </div>
+                <div>
+                    <Button type="success" rota="/login" label="RESETAR SENHA"/>
+                </div>
             </div>
         )
     }
--- src/app/containers/RecuperarSenha/index.js
+++ src/app/containers/RecuperarSenha/index.js
@@ -1,10 +1,42 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
+import Titulo from '../../components/Texto/Titulo';
+import Input from '../../components/Inputs/Simples';
+import Button from '../../components/Button/Simples';
+import { Link } from 'react-router-dom';
+
 
 class RecuperarSenha extends Component {
-    render(){
-        return(
+
+    state = {
+        email: ""
+    }
+
+    onChangeInput = (field, ev) => this.setState({ [field]: ev.target.value });
+
+    render() {
+        const { email } = this.state;
+        return (
             <div className="Recuperar-Senha">
-                Recuperar-senha
+                <Titulo tipo="h1" titulo="LOJA IT" />
+                <br />
+                <div>
+                    <p>Para reiniciar sua senha digite seu e-mail no campo abaixo</p>
+                    <p>Iremos enviar um link por e-mail para você acessar e entrar com uma nova senha!</p>
+                </div>
+                <br />
+                <div>
+                    <Input
+                        label="E-mail"
+                        value={email}
+                        onChange={(ev) => this.onChangeInput("email", ev)}
+                        type="email" />
+                </div>
+                <br/>
+                <div>
+                    <Button type="success" rota="/resetar-senha/1" label="RESETAR SENHA"/>
+                </div>
+
             </div>
         )
     }
